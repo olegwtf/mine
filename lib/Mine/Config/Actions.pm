e6a3d317795f46b5aded217c33e434a34978303e
Oleg G <verdrehung@gmail.com>
2011-05-09 02:02:09 +0700

    [ADD] Config::Actions - work with actions config. Still incomplete...

---
+++ lib/Mine/Config/Actions.pm
@@ -0,0 +1,17 @@
+package Mine::Config::Actions;
+
+use strict;
+use base Mine::Config::;
+
+=head1 NAME
+
+Mine::Config::Actions - mine actions config manipulator class
+
+=cut
+
+
+sub new {
+	
+}
+
+1;
